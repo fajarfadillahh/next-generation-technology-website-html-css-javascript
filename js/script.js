a13038542f26f62c6a225b3595d08655d41187cd
fajarfadillahh <fajaragustian979@gmail.com>
2021-05-21 11:39:20 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// SHOW & HIDDEN HEADER MENU
+const showMenu = (menuId, toggleId, closeId) => {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+  const close = document.getElementById(closeId);
+
+  if (menu && toggle && close) {
+    toggle.onclick = () => {
+      menu.classList.add("show-menu");
+    };
+    close.onclick = () => {
+      menu.classList.remove("show-menu");
+    };
+  }
+};
+showMenu("nav-menu", "nav-toggle", "nav-close");
